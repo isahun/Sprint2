7d4df0c76d39b557af118038e21072b1fccd05af
Irene <isahun7@gmail.com>
2025-12-15 13:53:48 +0100

    ex1 done

---
+++ 2.1-js-exercicis/script.js
@@ -0,0 +1,13 @@
+"use strict"
+
+// Exercici 1.1: ARROW FUNCTIONS
+
+// Nivell 1
+        
+// Exercici 1: Conversió de funcions: Tens una funció add que accepta dos paràmetres i retorna la seva suma. Converteix-la en una funció de fletxa. Per exemple: function add(a, b) {return a + b;}.
+
+const add = (a, b) =>  a + b;
+console.log(add(22, 11));
+
+//Exercici 2: Funció de fletxa sense paràmetres: Crea una funció de fletxa anomenada randomNumber que no necessiti paràmetres i que retorni un número aleatori entre 0 i 100.
+
