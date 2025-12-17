dcbdcfcfcc7f1592bbfcdabdf94dc68e473964b4
Irene Virgínia <isahun7@gmail.com>
2025-12-17 21:53:31 +0100

    bloc 1.5 array transformations done

--- 2.1-js-exercicis/script.js
+++ 2.1-js-exercicis/script.js
@@ -229,3 +229,163 @@ function suma(a, b, ...c) {
 
 console.log(suma(1,2,3,5,17));
 
+//Nivell 2
+
+//Exercici 3. Copiant objectes amb Spread: Crea un objecte 'objecte1'. Després crea un segon objecte, 'objecte2', que sigui una còpia de 'objecte1' utilitzant l'operador spread. Canvia una propietat de 'objecte2' i comprova que 'objecte1' no ha canviat.
+
+const objecte1 = {
+    size: 4,
+    weight: 3
+};
+
+console.log(objecte1);
+
+const objecte2 = {...objecte1} //spread creates a shallow copy
+
+objecte2.size = 45;
+
+console.log(objecte2);
+
+//Exercici 4. Rest en Destructuring: Crea una array amb diversos elements. Utilitza destructuring i l'operador rest per a assignar els primers dos elements a variables, i després assignar la resta dels elements a una tercera variable.
+
+const fellowship = ["Frodo", "Gandalf", "Aragorn", "Merry", "Pippin", "Gimli", "Legolas"];
+
+const [frodo, gandalf, ...mellon] = fellowship;
+
+console.log(frodo);
+console.log(gandalf);
+console.log(mellon);
+
+//Nivell 3
+
+//Exercici 5. Spread en Funcions: Crea una funció que accepti tres arguments. Després, crea una array amb tres elements i crida la funció utilitzant l'operador spread amb aquesta array.
+
+function triple (a, b, c) {
+    console.log(a, b, c);
+}
+
+const fruites = ["Maduixa", "Cirera", "Mandarina"];
+triple(...fruites);
+
+//Exercici 6. Fusionant Objectes amb Spread: Crea dos objectes amb propietats diferents. Utilitza l'operador spread per a fusionar aquests dos objectes en un de nou.
+
+const peonia = {
+    color: "Rosa",
+    familia: "Paeoniaceae"
+};
+
+const rosa = {
+    espines: true,
+    fulla: "aspra"
+};
+
+const flors = {... peonia, ...rosa};
+console.log(flors);
+
+//EXERCICI 1.5: ARRAY TRANSFORMATIONS
+
+//Nivell 1
+
+//Exercici 1. Map: Teniu un array de números [1, 2, 3, 4]. Crea una nova array que contingui el quadrat de cada número.
+
+const primeNums = [1, 3, 5, 7, 11];
+const primeSq = primeNums.map(square);
+
+function square(number) {
+    return number * number;
+}
+
+console.log(primeNums);
+console.log(primeSq);
+
+//Exercici 2. Filter: Teniu una array de números [1, 2, 3, 4]. Crea una nova array que només contingui els números parells.
+
+const arrayFour = [1, 2, 3, 4];
+const arrayPair = arrayFour.filter(findPairs);
+
+function findPairs (number) {
+    return number % 2 == 0;
+}
+
+console.log(arrayFour);
+console.log(arrayPair);
+
+//Exercici 3. Find: Teniu una array de números [1, 10 , 8, 11]. Utilitza la funció find per a trobar el primer número que és major a 10.
+
+const arrayTen = [1, 10 , 8, 11];
+const arrayOverTen = arrayTen.find(findTen);
+
+function findTen (number) {
+    return number > 10;
+}
+
+console.log(arrayTen);
+console.log(arrayOverTen);
+
+//Exercici 4. Reduce: Teniu una array de números [13, 7, 8, 21]. Fes servir la funció reduce per a calcular la suma total dels números.
+
+const arrayReduce = [13, 7, 8, 21];
+
+const result = arrayReduce.reduce(numberSum);
+
+function numberSum (total, number) { //total és el valor començant x l'esquerra
+    return total + number;
+}
+
+console.log(arrayReduce);
+console.log(result);
+
+//Nivell 2
+
+//Exercici 5. Donat un array "[ 1, 3, 7, 10 ,15, 17, 11, 5, 8, 12, 9 ]", crea una funció en una sola línia que faci el següent: 
+//- Filtra els nombres majors o iguals a 10. 
+// - Multiplica cada nombre filtrat per 2. 
+// - Calcula la suma dels nombres filtrats i multiplicats per 2. 
+// - La funció ha de retornar el resultat de la suma.
+
+const arrayEx5 = [1, 3, 7, 10 ,15, 17, 11, 5, 8, 12, 9]; 
+
+function processNums (array) {
+    return ((array.filter((number) => number >= 10)).map((number) => number * 2)).reduce((total,  number) => total + number);
+}
+
+console.log(processNums(arrayEx5));
+
+//Nivell 3
+
+//Exercici 6. Every / Some: Usa every i some per a determinar si tots o alguns dels elements de l'array [11, 12, 13, 14] són majors que 10, respectivament.
+
+const arrayEx6 = [11, 12, 13, 14];
+
+function checkTen (number) {
+    return number > 10;
+}
+
+console.log(arrayEx6.every(checkTen));
+
+//EXERCICI 1.6: ARRAY LOOPS
+
+//Nivell 1
+
+//Exercici 1. forEach: Teniu una array de noms. Utilitza forEach per a imprimir cada nom a la consola: let noms = ['Anna', 'Bernat', 'Clara'];
+
+
+
+//Exercici 2. for-of: Teniu una array de noms. Utilitza un bucle for-of per a imprimir cada nom a la consola: let noms = ['Anna', 'Bernat', 'Clara'];
+
+//Exercici 3. filter: Teniu una array de números. Utilitza filter per a crear una nova array que només contingui els números parells. let numeros = [1, 2, 3, 4, 5, 6];
+
+
+//Nivell 2
+
+//Exercici 4. for-in: Teniu un objecte amb parells clau-valor: let obj = { nom: Ona, edat: 25, ciutat: 'Barcelona' }; Utilitza un bucle for-in per a imprimir a la consola cada clau i el seu valor corresponent.
+
+//Exercici 5. for-of amb break: Teniu una array de números. Utilitza un bucle for-of per a imprimir a la consola els números fins a trobar el número 5, llavors atura el bucle: let numeros = [1, 2, 3, 4, 5, 6];
+
+
+//Nivell 3
+
+//Exercici 6. for-of amb index: Utilitza un bucle for-of per a imprimir a la consola cada element de l'array i la seva posició (index): let noms = ['Anna', 'Bernat', 'Clara']
+
+
+
