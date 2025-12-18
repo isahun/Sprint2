4eedd43388fb51770ed49fe985decda1ad002226
Irene <isahun7@gmail.com>
2025-12-18 13:55:05 +0100

    array methods mini advance

--- 2.1-js-exercicis/script.js
+++ 2.1-js-exercicis/script.js
@@ -369,18 +369,59 @@ console.log(arrayEx6.every(checkTen));
 
 //Exercici 1. forEach: Teniu una array de noms. Utilitza forEach per a imprimir cada nom a la consola: let noms = ['Anna', 'Bernat', 'Clara'];
 
+let noms = ["Anna", "Bernat", "Clara"];
 
+noms.forEach(printNames);
+
+function printNames(nom) {
+    console.log(nom);
+}
 
 //Exercici 2. for-of: Teniu una array de noms. Utilitza un bucle for-of per a imprimir cada nom a la consola: let noms = ['Anna', 'Bernat', 'Clara'];
 
+let print = "";
+for (let x of noms) {
+    print += x + "\n";
+}
+
+console.log(print);
+
 //Exercici 3. filter: Teniu una array de números. Utilitza filter per a crear una nova array que només contingui els números parells. let numeros = [1, 2, 3, 4, 5, 6];
 
+let numeros =  [1, 2, 3, 4, 5, 6];
+
+const numerosPair = numeros.filter((number) => number % 2 == 0)
+
+console.log(numerosPair);
 
 //Nivell 2
 
 //Exercici 4. for-in: Teniu un objecte amb parells clau-valor: let obj = { nom: Ona, edat: 25, ciutat: 'Barcelona' }; Utilitza un bucle for-in per a imprimir a la consola cada clau i el seu valor corresponent.
 
+let obj = {
+    nom: "Ona",
+    edat: "25",
+    ciutat: "Barcelona"
+}
+
+let message = "";
+
+for (let x in obj){
+    message += obj[x] + "\n";
+}
+
+console.log(message);
+
 //Exercici 5. for-of amb break: Teniu una array de números. Utilitza un bucle for-of per a imprimir a la consola els números fins a trobar el número 5, llavors atura el bucle: let numeros = [1, 2, 3, 4, 5, 6];
+
+let anotherNumArray = [1, 2, 3, 4, 5, 6];
+
+let printed = ""
+for (let x of anotherNumArray) {
+    if (x === 5) break;
+    
+}
+
 
 
 //Nivell 3
